3c0d6f98e65760071d9c48e94fe13ed2d07c63b6
Antoine <antoine.bonnotte@gmail.com>
2022-05-07 15:45:54 +0200

    ajout de la légende pixel non ?

--- src/view/LegendWindow.cpp
+++ src/view/LegendWindow.cpp
@@ -8,5 +8,48 @@ LegendWindow::LegendWindow()
     }
 
 void LegendWindow::draw() {
+     wattron(_win,A_BOLD);
+
     
+        mvwprintw(_win,0,3,"LEGENDE: ");
+    
+    
+        mvwprintw(_win,0,20," \(vide\)-> l'eau");
+
+        mvwprintw(_win,0,40," : -> la terre");
+    
+
+        mvwprintw(_win,0,60," [1-5] ->  choix spawn du virus ");
+
+    // légende des villes
+     
+        wattron(_win,COLOR_PAIR(3));
+                mvwprintw(_win,0,100,"[A-Z]");
+        wattroff(_win,COLOR_PAIR(3));
+        mvwprintw(_win,0,105," -> les villes");
+
+        wattron(_win,COLOR_PAIR(4));
+                mvwprintw(_win,0,130,"J");
+        wattroff(_win,COLOR_PAIR(4));
+
+        wattron(_win,COLOR_PAIR(4));
+                mvwprintw(_win,0,133,"D");
+        wattroff(_win,COLOR_PAIR(4));
+
+         wattron(_win,COLOR_PAIR(4));
+                mvwprintw(_win,0,136,"C");
+        wattroff(_win,COLOR_PAIR(4));
+
+        mvwprintw(_win,0,138," -> les villes qui correspondent entre elles");
+
+
+   
+    // éléments infectées 
+
+        wattron(_win,COLOR_PAIR(11));
+                mvwprintw(_win,0,190,"[A-Z][1-5][:]");
+        wattroff(_win,COLOR_PAIR(11));
+        mvwprintw(_win,0,203," -> les éléments infectés");
+     wattron(_win,A_BOLD);
+
 }
